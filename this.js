65b8bd42d4f4f438f5a963108669fa60a4396fc3
Ankit Saklani <saklanis960@gmail.com>
2023-10-12 00:24:38 +0530

    summary

--- this.js
+++ this.js
@@ -1,1 +1,14 @@
-console.log('hello');
+// console.log('hello');
+const a = 1;
+let b = 2;
+var c = 3;
+
+// a = 4;
+// console.log(a);
+
+// console.table([a, b, c]);
+
+// console.log(a);
+// console.table([a]);
+
+console.log(typeof null);
