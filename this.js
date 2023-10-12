8e2dd2185cc162cb2ec714063129c2198b403620
Ankit Saklani <saklanis960@gmail.com>
2023-10-12 23:55:35 +0530

    summary

--- this.js
+++ this.js
@@ -1,7 +1,7 @@
 // console.log('hello');
-const a = 1;
-let b = 2;
-var c = 3;
+// const a = 1;
+// let b = 2;
+// var c = 3;
 
 // a = 4;
 // console.log(a);
@@ -11,4 +11,29 @@ var c = 3;
 // console.log(a);
 // console.table([a]);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// const a='ankitsaklani';
+// console.log(a);
+// const c = a.substring(-8, 5);
+// console.log(c);
+// const b = a.slice(1, -4);
+// console.log(b);
+
+// console.log(null == 0);
+// console.log(null >= 0);
+console.log(undefined == null);
+const gameName = new String('The Legend of Zelda');
+console.log(gameName);
+// {
+//     a= 1;
+//     console.log(a);
+// }
+gameName;
+let rahul;
+let rahuls= null;
+console.log(rahul);
+console.log(rahuls);
+console.log(rahul === rahuls);
+
+a=10;
