f319125236eb6c112bca99ca5daefc8f5eb7fa8c
Ankit Saklani <saklanis960@gmail.com>
2023-10-13 13:13:20 +0530

    summary

--- this.js
+++ this.js
@@ -40,4 +40,43 @@
 
 const min = 1
 const max = 7
-console.log(Math.floor(Math.random() * (max - min)) + min);
+// console.log(Math.floor(Math.random() * (max - min)) + min);
+
+let mydate = new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toDateString());
+
+// console.log(mydate.getDay());
+// console.log(typeof mydate);
+
+let mytime = new Date("01-14-2023");
+// console.log(mytime.toLocaleString());
+
+let mytimestamp = Date.now()
+
+
+// console.log(Math.floor(mytimestamp/1000));
+
+// newDate.toLocaleStringa( { timeZone: 'en-US' });
+// console.log(newDate);
+
+const arr = [11, 2, 3, 4, 55, 6, 7, 8, 9, 10];
+
+const newArr = arr.map(function (x) {
+    return x * 2;
+})
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+console.log(arr.reverse());
+console.log(typeof arr);
+
+const newArrm = arr.join();
+console.log(newArrm);
+console.log(typeof newArrm);
